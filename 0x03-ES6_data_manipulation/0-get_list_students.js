95768057160e0d1850c6ee2b318026bde3e4c8ac
Jayneey <sauceqwinn@gmail.com>
2023-03-27 21:33:44 +0000

    getliststudents

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Jane Affam  <https://github.com/Jayneey> 
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
