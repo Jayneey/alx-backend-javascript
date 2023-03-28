9df14952f523dc80706842042d0655343c837eee
Jane Affam <75577542+Jayneey@users.noreply.github.com>
2023-03-27 17:03:53 -0700

    Uniq items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@
  * Changes the quantity of unique grocery items to 100.
  * @param {Map<String, number>} map - A map of the name of a
  * grocery and its quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Jane Affam <https://github.com/Jayneey>
  */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
