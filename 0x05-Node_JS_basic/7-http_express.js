3e25a53eb5c0526067ae57ad119584152db2e7db
Jane Affam <75577542+Jayneey@users.noreply.github.com>
2023-06-22 03:30:11 -0700

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,29 +1,37 @@
 const express = require('express');
-const students = require('./3-read_file_async');
+const fs = require('fs');
+const { countStudents, readDatabase } = require('./3-read_file_async');
+
 const app = express();
-const hostname = '127.0.0.1';
 const port = 1245;
 
+// Root endpoint
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
+// Students endpoint
 app.get('/students', async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
-    res.write(`Number of students: ${data.students.length}\n`);
-    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => res.write(err.message))
-    .finally(() => {
-      res.end();
-    });
+  const database = process.argv[2];
+
+  try {
+    const data = await readDatabase(database);
+    const { students, csStudents, sweStudents } = countStudents(data);
+
+    const response = `This is the list of our students
+Number of students: ${students.length}
+Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}
+Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`;
+
+    res.send(response);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
+
+module.exports = app;
